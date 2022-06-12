137cc9e3c250e187277939fd23756badb805d754
Jonathan <Jjh201104@gmail.com>
2022-06-12 10:40:11 +0100

    refactor: class stores broken cases

--- src/breakthis.py
+++ src/breakthis.py
@@ -6,28 +6,28 @@ class BreakThis:
     def __init__(self, expected_exception: type(BaseException), function):
         self.expected_exception = expected_exception
         self.function = function
+        self.broken_cases = []
 
-    def break_with_primitive_zero_types(self):
-        arg_list = self.get_arg_list()
+    def breaker(self):
+        self._break_with_primitive_zero_types()
 
-        broken_cases = []
+    def _break_with_primitive_zero_types(self):
+        arg_list = self._get_arg_list()
+
         for args in arg_list:
             try:
                 self.function(*args)
             except self.expected_exception:
                 pass
             except Exception:
-                broken_cases.append(args)
+                self.broken_cases.append(args)
 
-        return broken_cases
-
-    def get_arg_list(self):
-        no_args = self.get_no_args()
+    def _get_arg_list(self):
+        no_args = self._get_no_args()
         params = [0, 0.0, False, "", [], {}, None]
-        arg_list = self.build_arg_list(no_args, params)
-        return arg_list
+        return self._build_arg_list(no_args, params)
 
-    def build_arg_list(self, no_args, params):
+    def _build_arg_list(self, no_args, params):
         arg_list = []
 
         for i in range(no_args):
@@ -36,7 +36,7 @@ class BreakThis:
         return list(itertools.product(*arg_list))
 
 
-    def get_no_args(self):
+    def _get_no_args(self):
         details = signature(self.function)
         no_args = len(details.parameters)
         return no_args
--- tests/test_break.py
+++ tests/test_break.py
@@ -14,12 +14,12 @@ class MyCustomException(BaseException):
 class TestBreak(unittest.TestCase):
     def test__try_basic_types__returns_success_list(self):
         break_this = BreakThis(expected_exception=MyCustomException, function=add)
-        errors = break_this.break_with_primitive_zero_types()
-        self.assertIsInstance(errors, list)
-        self.assertEqual(len(errors), 38)
+        break_this._break_with_primitive_zero_types()
+        self.assertIsInstance(break_this.broken_cases, list)
+        self.assertEqual(len(break_this.broken_cases), 38)
 
     def test_build_arg_list_builds_all_combinations(self):
         break_this = BreakThis(expected_exception=MyCustomException, function=add)
         params = ["p1", "p2"]
-        arg_list = break_this.build_arg_list(no_args=3, params=params)
+        arg_list = break_this._build_arg_list(no_args=3, params=params)
         self.assertEqual(len(arg_list), 8)
